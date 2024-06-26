cc3fec8eaae620c9389e8e6fd741c3661f7c0379
heavencandle <whj0210@gmail.com>
2024-06-27 01:04:09 +0900

    Refactor layout

--- app.py
+++ app.py
@@ -1,4 +1,7 @@
 # TODO; Add minimum, fix logic, change chart, design, llm 붙여서 개선하려면 어떻게 해야하는지? 무엇이 문제인지 도출, pill with question
+# zero division, placeholder
+# llm for key insight
+# Change to month label
 
 import streamlit as st
 import pandas as pd
@@ -36,51 +39,53 @@ def project_mau(month, initial_population, vector):
     mau_by_age["month"] = [m for m in range(month+1)]
     for age, row in enumerate(np.transpose(mau_list)):
         mau_by_age[f"age_{age}"] = row
-    
-    st.write(mau_by_month)
-    st.write(mau_by_age)
 
     return mau_by_month, mau_by_age
 
-with st.container(border=True): 
+with st.container(): 
     st.write("MAU Projection")
 
-with st.container(border=True): 
+with st.container(): 
     col1, col2 = st.columns(2)
 
-    user_age = int(col1.number_input("1. User Age", step=1, key="user_age"))
+    with col1:
+        user_age = int(st.number_input("1. User Age", step=1, key="user_age"))
 
-    month = int(col1.number_input("2. Month", step=1, key="month"))
+        month = int(st.number_input("2. Month", step=1, key="month"))
 
-    initial_population = [col1.number_input("2. Initial Population", step=1, key=f"initial_population_0")]
-    for age in range(0, user_age):    
-        initial_population.append(col1.number_input("", step=1, label_visibility ="collapsed", 
-                                                    key=f"initial_population_{age+1}"))
+        initial_population = [st.number_input("2. Initial Population", step=1, key=f"initial_population_0")]
+        for age in range(0, user_age):    
+            initial_population.append(st.number_input("", step=1, label_visibility ="collapsed", 
+                                                        key=f"initial_population_{age+1}"))
 
-    inflow = col1.number_input("3. Inflow", step=0.01, key=f"inflow")
-    retention = [col1.number_input("4. Retention", step=0.01, min_value=0.0, max_value=1.0, key=f"retention_0")]
-    for m in range(1, user_age):    
-        retention.append(col1.number_input("", step=0.01, min_value=0.0, max_value=1.0, label_visibility ="collapsed", 
-                                           key=f"retention_{m}"))
+        inflow = st.number_input("3. Inflow", step=0.01, key=f"inflow")
+        retention = [st.number_input("4. Retention", step=0.01, min_value=0.0, max_value=1.0, key=f"retention_0")]
+        for m in range(1, user_age):    
+            retention.append(st.number_input("", step=0.01, min_value=0.0, max_value=1.0, label_visibility ="collapsed", 
+                                            key=f"retention_{m}"))
+    with col2:
+        st.write(f"""MAU over {month} months""")
+        toggle_on = st.toggle("Show by user age")
 
-    col2.write(f"""MAU over {month} months""")
-    toggle_on = col2.toggle("Show by user age")
+        vector = calc_vector(inflow, retention)
+        mau_by_month, mau_by_age = project_mau(month, initial_population, vector)
 
-    vector = calc_vector(inflow, retention)
-    mau_by_month, mau_by_age = project_mau(month, initial_population, vector)
+        mau_by_month = pd.DataFrame(mau_by_month)
+        mau_by_age = pd.DataFrame(mau_by_age)
 
-    mau_by_month = pd.DataFrame(mau_by_month)
-    mau_by_age = pd.DataFrame(mau_by_age)
+        if toggle_on: 
+            # by user age
+            st.line_chart(mau_by_age, x="month", y=[v for v in mau_by_age.columns.values if v!= "month"])
+        else:
+            # by total
+            st.line_chart(mau_by_month, x="month", y="mau")
+        st.write(f"""Key insight zone""")
 
-    st.write(mau_by_month)
-    st.write(mau_by_age)
-
-    if toggle_on: 
-        # by user age
-        col2.line_chart(mau_by_age, x="month", y=[v for v in mau_by_age.columns.values if v!= "month"])
-    else:
-        # by total
-        col2.line_chart(mau_by_month, x="month", y="mau")
+with st.container():
+    with st.expander("See calculation"):
+        st.write('''
+            describe how it was calculated
+        ''')
 
 
 
